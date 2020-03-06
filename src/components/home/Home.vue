d08cd514c433bbd402a7ecf23777f9e58b100e75
liuxv <1721518931@qq.com>
2020-03-06 16:50:05 +0800

    图标区域

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <Header></Header>
     <Swiper></Swiper>
+    <Icons></Icons>
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
@@ -9,11 +10,13 @@
 <script>
 import Header from "components/home/Header";
 import Swiper from "components/home/Swiper";
+import Icons from "components/home/Icons";
 export default {
   name: "Home",
   components: {
     Header,
-    Swiper
+    Swiper,
+    Icons
   }
 };
 </script>
---
+++ src/components/home/Icons.vue
@@ -0,0 +1,149 @@
+<template>
+  <div class="icons">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="icon in page" :key="icon.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="icon.imgUrl" alt="酒店" />
+          </div>
+          <p class="icon-desc">{{icon.imgDesc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "icons",
+  data() {
+    return {
+      swiperOption: {
+        autoplay: true,
+        pagination: {
+          el: ".swiper-pagination"
+        }
+      },
+      iconsList: [
+        {
+          id: "001",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          imgDesc: "景点门票"
+        },
+        {
+          id: "002",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png",
+          imgDesc: "一日游"
+        },
+        {
+          id: "003",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png",
+          imgDesc: "滑雪季"
+        },
+        {
+          id: "004",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png",
+          imgDesc: "泡温泉"
+        },
+        {
+          id: "005",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/fc/b10a6b2e4f0fe102.png",
+          imgDesc: "动物园"
+        },
+        {
+          id: "006",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          imgDesc: "植物园"
+        },
+        {
+          id: "007",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          imgDesc: "游乐园"
+        },
+        {
+          id: "008",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          imgDesc: "民宿"
+        },
+        {
+          id: "009",
+          imgUrl:
+            "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+          imgDesc: "民宿"
+        }
+      ]
+    };
+  },
+  computed: {
+    pages() {
+      const pages = [];
+      this.iconsList.forEach((item, index) => {
+        const page = Math.floor(index / 8);
+        if (!pages[page]) {
+          pages[page] = [];
+        }
+        pages[page].push(item);
+      });
+      return pages;
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+@import '~styles/mixins.styl';
+
+// icons使用浮动布局，8个icon自动分布2行，icon内图片和描述使用子绝父相
+.icons {
+  width: 100%;
+  overflow: hidden;
+  height: 0;
+  padding-bottom: 50%;
+
+  .icon {
+    position: relative;
+    overflow: hidden;
+    float: left;
+    width: 25%;
+    height: 0;
+    padding-bottom: 25%;
+
+    .icon-img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0.44rem;
+      box-sizing: border-box;
+      padding: 0.1rem;
+
+      .icon-img-content {
+        display: block;
+        margin: 0 auto;
+        height: 100%;
+      }
+    }
+
+    .icon-desc {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      line-height: 0.8rem;
+      height: 0.8rem;
+      color: $darkTextColor;
+      text-align: center;
+      ellipsis();
+    }
+  }
+}
+</style>

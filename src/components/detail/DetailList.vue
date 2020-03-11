d66baba4c171fd4c17271dab257593cb67ade278
liuxv <1721518931@qq.com>
2020-03-11 17:42:46 +0800

    递归组件

---
+++ src/components/detail/DetailList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div class="item" v-for="(item,index) of list" :key="index">
+      <div class="item-title border-bottom">
+        <span class="item-title-icon"></span>
+        {{item.title}}
+      </div>
+      <div v-if="item.children">
+        <detail-list :list="item.children" class="item-children"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "detailList",
+  props: {
+    list: Array
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.item-title-icon {
+  position: relative;
+  left: 0.06rem;
+  top: 0.06rem;
+  display: inline-block;
+  line-height: 2rem;
+  width: 0.4rem;
+  height: 0.4rem;
+  background: url('http://s.qunarzz.com/piao/image/touch/sight/detail.png') no-repeat;
+  margin-right: 0.1rem;
+  background-size: 0.4rem 3rem;
+}
+
+.item-title {
+  line-height: 3rem;
+  font-size: 1.2rem;
+  padding: 0 0.2rem;
+}
+
+.item-children {
+  padding: 0 1rem;
+}
+</style>
--- src/views/Detail.vue
+++ src/views/Detail.vue
@@ -2,6 +2,7 @@
   <div>
     <detail-banner></detail-banner>
     <detail-header></detail-header>
+    <detail-list :list="list"></detail-list>
     <div class="container"></div>
   </div>
 </template>
@@ -9,11 +10,41 @@
 <script>
 import DetailBanner from "components/detail/DetailBanner";
 import DetailHeader from "components/detail/DetailHeader";
+import DetailList from "components/detail/DetailList";
 export default {
   name: "detail",
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data() {
+    return {
+      list: [
+        {
+          title: "成人票",
+          children: [
+            {
+              title: "成人三馆联票",
+              children: [
+                {
+                  title: "成人三馆联票 - 某一连锁店销售"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          title: "学生票"
+        },
+        {
+          title: "儿童票"
+        },
+        {
+          title: "特惠票"
+        }
+      ]
+    };
   }
 };
 </script>
